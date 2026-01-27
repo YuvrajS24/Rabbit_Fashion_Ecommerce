03df6521cb79c644fc8f592ab37481a7bffbb18c
YuvrajS24 <yuvraj.sachdev2403@gmail.com>
2026-01-27 22:32:07 +0530

    27th Jan'26- Making Frontend of the Rabbit Ecommerce

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../Common/Header'
+import Footer from '../Common/Footer'
+
+const UserLayout = () => {
+  return (
+   
+    <>
+
+      {/* header*/ }
+      <Header/>
+
+      {/* Main Content*/}
+      {/* Footer*/}
+
+      <Footer/>
+
+    </>
+
+
+
+
+  )
+}
+
+export default UserLayout
