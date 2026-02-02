0a851f492bddbc5662d6e98f4585ac5851b8a6ca
YuvrajS24 <yuvraj.sachdev2403@gmail.com>
2026-02-02 22:51:02 +0530

    2nd Feb'26 - completed admin dashboard , started backend part of the website (Created user model and schema).

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+dotenv.config()
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+
+// Connect to MongoDB 
+
+
+connectDB();
+
+
+
+
+
+app.use("/",(req,res)=>{
+
+res.send('WELCOME TO RABBIT API!');
+
+
+})
+
+
+app.listen(PORT , ()=> {
+
+     console.log(`Server is running on http://localhost:${PORT}`);
+
+});
+
--- frontend/src/components/Admin/AdminSidebar.jsx
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -61,7 +61,7 @@ const AdminSidebar = () => {
             </NavLink>
 
 
-            <NavLink to="/admin/shop" className={({isActive}) => isActive ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2" 
+            <NavLink to="/" className={({isActive}) => isActive ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2" 
             : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2 "}>
 
             <FaStore/>
