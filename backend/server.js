53e07da93422d83c1a74fe39b9d3093e1ff55b9f
YuvrajS24 <yuvraj.sachdev2403@gmail.com>
2026-06-07 17:22:30 +0530

    security: add express-mongo-sanitize to prevent NoSQL injection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config()
 const express = require('express');
 const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes')
@@ -17,7 +19,7 @@ const adminOrderRoutes = require('./routes/adminOrderRoutes')
 
 
 const app = express();
-app.use(express.json());
+
 app.use(cors({
   origin: [
     "https://rabbitfashion.vercel.app",  
@@ -26,6 +28,9 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(express.json());
+
+app.use(mongoSanitize());
 
 
 
